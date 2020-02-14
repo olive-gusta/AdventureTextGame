dae99f353ce4372ee946362d833037cfa0276835
olive-gusta <daylen_vergomes12@hotmail.com>
2020-02-14 02:14:50 -0300

    Versão 0.1.1 Data: 14/02/2020 02:15 -3gmt
    Acabamos de organizar o código que nos permite mudar de coordenadas. O que permite que o jogador digite à coordenada
    desejada a partir da descrição contida na cena atual, e vá para a cena coorespondente a direção escolhida.

--- adventureScratch.py
+++ adventureScratch.py
@@ -1,15 +1,33 @@
-#import envMaker
-#import comandos
+import comandos as com
 import numpy as np
 
-arquivo = np.genfromtxt("cenas.txt", dtype=None, delimiter=";")
+arquivo = np.genfromtxt("cenas.txt", delimiter=";", comments="#", dtype=[('id',int), 
+                                            ('Text', "|U2400"), 
+                                            ('N',int), 
+                                            ('S',int), 
+                                            ('L',int), 
+                                            ('O',int), 
+                                            ('com',bool)])
 
 #precisamos de uma função que sempre escuta o que o jogador tem a dizer
-    def file_find(cenas):
+def file_find(cenas):
     return arquivo[cenas]
 
+cena = 0
+
 while True:
+    #apresentando a cena
+    cena_atual = com.Cena(file_find(cena))
+    print(cena_atual.texto_cena())
+    
+    #pegando a informaçãod do jogador
     action = input("> ")
-    cena
-    x = file_find(cena))
-    print(x)
+    action = com.Player_act(action)
+    action = action.acao()
+
+    #aqui verificamos se é uma ação de coordenada e mudamos para a cena correspondente aquela coordenada
+    if action in ['N', 'S', 'L', 'O']:
+        cena = cena_atual.mudar_coord(action)
+        print(cena)
+    else: print('error')
+
--- comandos.py
+++ comandos.py
@@ -1,44 +1,50 @@
 class Player_act():
     def __init__(self, input_com):
         self.input_com = input_com 
-        act = {
-               
-                "NORTE":N, "N":N,"SUL":S, "S":S, "LESTE":L, "L":L, "OESTE":O, "O":O, "ATACAR":att, "ATT":att
-                
+        self.act = {
+                "NORTE":'N', 
+                "N":'N', 
+                "SUL":'S', 
+                "S":'S', 
+                "LESTE":'L', 
+                "L":'L', 
+                "OESTE":'O', 
+                "O":'O', 
+                "ATACAR": 'att', 
+                "ATT": 'att'
                 }
 
-    def acao(self.input_com):
+    def acao(self):
         #ignorando aqui as letras minusculas
         self.input_com = self.input_com.upper().strip()
         
-        return act.get(input_com)
+        return self.act.get(self.input_com)
 
 
 
 class Cena():
-    def __init__(self, N=False, S=False, L=False, O=False, att=False):
-        self.N = N
-        self.S = S
-        self.L = L
-        self.O = O
-        self.att = att
-    #os nums são outras cenas
-    def N(num):
-        N = !N
-        return num
+    def __init__(self, file_cena):
+        self.file_cena = file_cena
+        self.coords = [self.file_cena[2],self.file_cena[3],self.file_cena[4],self.file_cena[5]]
 
-    def S(num):
-        S = !S
-        return num
+    def id_cena(self):
+        return self.file_cena[0]
 
-    def L(num):
-        L = !L
-        return num
+    def texto_cena(self):
+        return self.file_cena[1]
 
-    def O(num):
-        O = !O
-        return num
-
+    #essa função é um metodo para mudar para a coordenada passada
+    def mudar_coord(self, coord):
+        if coord == 'N':# and self.coords[0] != 0:
+            return self.coords[0]
+        if coord == 'S':# and self.coords[1] !=0:
+            return self.coords[1]
+        if coord == 'L':# and self.coords[2] != 0:
+            return self.coords[2]
+        if coord == 'O':# and self.coords[3] != 0:
+            return self.coords[3]
+        
+        
 #class Item():
     
 #class Criatura():
